3b50b9309d407c1c7c79105bb12a801ed541a1e4
Aneesh Bharath <me@aneeshbharath.com>
2025-10-10 09:03:05 +0400

    heroku changes

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 from app import create_app, db
+import os
 
 app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), debug=os.environ.get("FLASK_DEBUG", False))
